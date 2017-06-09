ef288026de812db2d04ce37c6ffb0f3841ff809a
Michaelmwirigi <mwirigimichael@gmail.com>
2017-06-09 15:46:22 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document)
+  .ready(function() {
+
+   
+    // create sidebar and attach to menu open
+    $('.navigation_modal')
+      .modal('attach events', '.toggle_modal', 'show')
+    ;
+    /////form accordion////////////
+
+  });
+  
